2bb849e47ae07c60a8461d7f4445faf0857cac5d
Arun Bhat <araghavendra@Aruns-MacBook-Pro.local>
2018-09-10 22:01:30 +0530

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+# Class NTP
+#
+#
+#
+class ntp (
+  $package_ensure = 'present',
+){
+  # package install 
+  package { 'ntp':
+    ensure   => $package_ensure,
+    provider => 'yum',
+  }
+  # confi files
+  file { '/etc/ntp.conf':
+    ensure => 'present',
+    source => '/tmp/ntp.conf',
+  }
+  # manage service 
+  service { 'ntpd':
+    ensure => 'running',
+    enable => true,
+  }
+}
